c108b0736a8b8c3c437b43ea594b8835cfcb9519
LAPTOP-NMG7USOP\gotjs <gotjs94@naver.com>
2021-01-30 19:54:18 +0900

    챕터3 까지 공부한 내용

---
+++ 숫자야구/NumberBaseball.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+    <h1>{{result}}</h1>
+    <form v-on:submit="onSubmitForm">
+        <input ref="answer" maxlength="4" v-model="value" />
+        <button type="submit">입력</button>
+    </form>
+    <div>시도 : {{tries.length}}</div>
+    <ul>
+        <li v-for="t in tries">
+            <div>{{t.try}}</div>
+            <div>{{t.result}}</div>
+        </li>
+    </ul>
+    </div>    
+</template>
+<script>
+export default {
+    data (){ 
+        return{
+            tries: [],
+            value:'',
+            result:''
+        }
+    },
+    methods:{
+        onSubmitForm(e){
+            e.preventDefault();
+            this.tries.push({
+                try:this.value,
+                result:'홈런'
+            });
+            this.value = '';
+            this.$refs.answer.focus();
+        }
+    }
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
